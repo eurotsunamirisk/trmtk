2581e11bb0acb2317137b69ccd50459ce86f206c
eurotsunamirisk <88541668+eurotsunamirisk@users.noreply.github.com>
2021-12-15 19:06:49 +0100

    corrections

--- trmtk/plot_fragility_curve.py
+++ trmtk/plot_fragility_curve.py
@@ -9,12 +9,11 @@ DISCLAIMER
 
 This software is made available as a prototype implementation on behalf of
 scientists and engineers for the purpose of open collaboration and in the
-hope that it will be useful. It is not developed to the design standards, nor
-subject to same level of critical review by professional software
-developers, as GEM’s OpenQuake software suite. It is therefore distributed
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-for more details.
+hope that it will be useful. It is not developed to design standards, nor
+subject to critical review by professional software developers. It is
+therefore distributed WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
 The European Tsunami Risk Service, and the authors of the software, assume no
 liability for use of the software.
